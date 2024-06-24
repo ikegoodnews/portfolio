586505915e99eff11f0c31474ea0a065659bcb83
Goodnews Ike <goodnewsike19@gmail.com>
2024-06-24 11:48:34 +0100

    feat: updates

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,7 +6,9 @@ const Home = () => {
    return (
       <Layout title="Home" className="hero__section d-flex align-items-center justify-content-center">
          <div className="welcome text-start">
-            <h1 className="name pt-1 text-uppercase">I&apos;m goodnews ike</h1>
+            <h1 className="name pt-1 text-uppercase">
+               I&apos;m <span className="text-nowrap">goodnews ike</span>
+            </h1>
             <p className="description py-3">
                Highly proficient software developer with over 3 years of professional experience in developing robust and scalable web applications.
                My expertise lies in full-stack development with a keen focus on front-end technologies and user experience. I am committed to
