5aaf5cdca8d726c7b3ad2f408c10dea35ef0612f
Goodnews Ike <goodnewsike19@gmail.com>
2024-07-01 13:19:05 +0100

    pushing udates

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,6 +1,5 @@
 import {PROJECTS} from '@/_components/AppData';
 import {Layout, Title} from '@/_components';
-import {techStacks} from '@/_constants';
 import Image from 'next/image';
 import React from 'react';
 
@@ -11,42 +10,40 @@ const Projects = () => {
             <Title label="my work" title="projects" />
             <div className="row mt-5 gx-5">
                {PROJECTS?.map((project, i) => (
-                  <div key={i} className="col-lg-6 mb-md-5 mb-4">
-                     <div className="position-relative">
-                        <div className="projectCard h-100">
-                           <div className="img_wrapper">
-                              {/* <iframe src="https://app.waystream.io" frameborder="0" className="h-100 w-100"></iframe> */}
-                              <Image
-                                 src={project?.img}
-                                 className="d-block w-100"
-                                 alt={`${project?.title}-artwork`}
-                                 // quality={100}
-                                 unoptimized
-                                 height={100}
-                                 width={100}
-                                 priority
-                              />
-                           </div>
+                  <div key={i} className="col-lg-6 mb-md-5 mb-4 position-relative">
+                     <div className="projectCard h-100">
+                        <div className="img_wrapper">
+                           {/* <iframe src="https://app.waystream.io" frameborder="0" className="h-100 w-100"></iframe> */}
+                           <Image
+                              src={project?.img}
+                              className="d-block w-100"
+                              alt={`${project?.title}-artwork`}
+                              // quality={100}
+                              unoptimized
+                              height={100}
+                              width={100}
+                              priority
+                           />
+                        </div>
 
-                           <div className="details p-sm-4 p-2">
-                              <h5 title={project?.title} data-toggle="tooltip" data-placement="top" className="project__title pb-0 text-truncate">
-                                 {project?.title}
-                              </h5>
-                              <p
-                                 title={project?.description}
-                                 data-toggle="tooltip"
-                                 data-placement="top"
-                                 className="project__description pb-3 pt-2 text-truncate">
-                                 {project?.description}
-                              </p>
-                              <div className="techs__used overflow-hidden">
-                                 <div className="d-flex align-items-center">
-                                    {project?.techStacks?.slice(0, 5)?.map((stack, i) => (
-                                       <div key={i} className="stack me-2">
-                                          {stack}
-                                       </div>
-                                    ))}
-                                 </div>
+                        <div className="details p-sm-4 p-2">
+                           <h5 title={project?.title} data-toggle="tooltip" data-placement="top" className="project__title pb-0 text-truncate">
+                              {project?.title}
+                           </h5>
+                           <p
+                              title={project?.description}
+                              data-toggle="tooltip"
+                              data-placement="top"
+                              className="project__description pb-3 pt-2 text-truncate">
+                              {project?.description}
+                           </p>
+                           <div className="techs__used overflow-hidden">
+                              <div className="d-flex align-items-center">
+                                 {project?.techStacks?.slice(0, 5)?.map((stack, i) => (
+                                    <div key={i} className="stack me-2">
+                                       {stack}
+                                    </div>
+                                 ))}
                               </div>
                            </div>
                         </div>
